b117c53456db701f86a0a1f8d47d6575c926b829
Htut <phyo.htut@windriver.com>
2020-03-28 03:44:45 -0700

    TODO: app.py route 'api/upload'

--- react-frontend/src/components/DraggableUploader.js
+++ react-frontend/src/components/DraggableUploader.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from 'react';
 
 import { AnchorButton, Intent, ProgressBar } from "@blueprintjs/core";
 
@@ -9,13 +9,13 @@ import { remove } from 'react-icons-kit/fa/remove';
 import './assets/DraggableUploader.scss'
 import axios from 'axios'
 import Table from './Table'
-export default class DraggableUploader extends React.Component {
 
+class DraggableUploader extends Component {
   constructor(props) {
     super(props);
     this.state = {
       loadedFiles: [],
-      fd: '',
+      fd: null,
       confidence: '',
       diagnosis: '',
       dataObject: {
@@ -24,34 +24,50 @@ export default class DraggableUploader extends React.Component {
         Diagnosis: '',        
       },
     };
+
+    this.onFileLoad = this.onFileLoad.bind(this)
+    this.addLoadedFile = this.addLoadedFile.bind(this)
+    this.removeLoadedFile = this.removeLoadedFile.bind(this)
+    this.removeAllLoadedFile = this.removeAllLoadedFile.bind(this)
+    this.updateLoadedFile = this.updateLoadedFile.bind(this)
+    this.uploadFiles = this.uploadFiles.bind(this)
   }
 
   onFileLoad(e) {
-    const file = e.currentTarget.files[0];
-    let fd = new FormData();
-    console.log(fd);
-    let fileReader = new FileReader();
-    fd.append('image', file, file.name)
-    this.setState({ fd: fd })
-    fileReader.onload = () => {
-      console.log("IMAGE LOADED: ", fileReader.result);
-      const file = {
-        data: fileReader.result,
-        isUploading: false
+    let file = null
+    let fd = this.state.fd || new FormData()
+    
+    for (let i = 0; i < e.currentTarget.files.length; i++) {
+      file = e.currentTarget.files[i]
+      
+      if (file) {
+        let fileReader = new FileReader()
+        fd.append('image', file, file.name)
+    
+        fileReader.onload = () => {
+          const file = {
+            data: fileReader.result,
+            isUploading: false
+          }
+          //Add file
+          this.addLoadedFile(file);
+        }
+    
+        fileReader.onabort = () => {
+          alert("Reading Aborted");
+        }
+    
+        fileReader.onerror = () => {
+          alert("Reading ERROR!");
+        }
+    
+        fileReader.readAsDataURL(file);
       }
-      //Add file
-      this.addLoadedFile(file);
-    }
 
-    fileReader.onabort = () => {
-      alert("Reading Aborted");
+      if (i == e.currentTarget.files.length - 1) {
+        this.setState({ fd })
+      }
     }
-
-    fileReader.onerror = () => {
-      alert("Reading ERROR!");
-    }
-
-    fileReader.readAsDataURL(file);
   }
 
   addLoadedFile(file) {
@@ -80,64 +96,62 @@ export default class DraggableUploader extends React.Component {
 
   updateLoadedFile(oldFile, newFile) {
     this.setState((prevState) => {
+      const loadedFiles = [ ...prevState.loadedFiles ]
 
-      const loadedFiles = [...prevState.loadedFiles];
       _.find(loadedFiles, (file, idx) => {
-        if (file == oldFile)
-          loadedFiles[idx] = newFile;
-      }
-      );
+        if (file === oldFile) {
+          loadedFiles[idx] = newFile
+        }
+      })
 
       return { loadedFiles };
-    });
+    })
 
     return newFile;
   }
 
-  onUpload  = () => {
+  uploadFiles() {
     const { loadedFiles, fd } = this.state;
-    console.log('ON UPLOAD...')
-    loadedFiles.map((file, idx) => {
-      console.log("Updating...");
-      //Update file (Change it's state to uploading)
-      let newFile = this.updateLoadedFile(file, {
-        ...file,
-        isUploading: true
-      });
 
-      axios.post('/api/upload', fd)
-        .then(res => {
-          console.log(res);
-          let filename = res.data;
-          axios.get('/api/predict?fileName=' + filename)
-            .then(res => {
-              console.log('hi');
-              console.log(res);
-              let diagnosis = res.data.diagnosis
-              let confidence = res.data.confidence
-              this.setState({
-                dataObject: {
-                  Confidence: confidence,
-                  Diagnosis: diagnosis,
-                  Patient_ID: '1',
-                }
-              })
-            })
-        })
-        .catch(err => {
-          console.log(err);
-        })
+    let n = loadedFiles.length
 
-      //Simulate a REAL WEB SERVER DOING IMAGE UPLOADING
-      setTimeout(() => {
-        //Get it back to it's original State
-        this.updateLoadedFile(newFile, {
-          ...newFile,
-          isUploading: false
-        });
-      }, 3000);
+    for (let i = 0; i < n; i++) {
+      let file = loadedFiles.shift()
 
-    });
+      // let newFile = this.updateLoadedFile(file, {
+      //   ...file,
+      //   isUploading: true
+      // })
+
+      // TODO: handle the array of data from fd in the backend
+      // axios.post('/api/upload', fd).then(res => {
+      //   let filename = res.data;
+
+      //   axios.get('/api/predict?fileName=' + filename).then(res => {
+      //     let diagnosis = res.data.diagnosis
+      //     let confidence = res.data.confidence
+
+      //     this.setState({
+      //       dataObject: {
+      //         Confidence: confidence,
+      //         Diagnosis: diagnosis,
+      //         Filename: filename,
+      //       }
+      //     })
+
+      //     this.updateLoadedFile(newFile, {
+      //       ...newFile,
+      //       isUploading: false
+      //     })
+      //   })
+      // }).catch(err => {
+      //   console.log(err);
+      // })
+
+      if (i === n - 1) {
+        this.setState({ loadedFiles: [ ...loadedFiles ] })
+      }
+    }
   }
 
   render() {
@@ -146,59 +160,83 @@ export default class DraggableUploader extends React.Component {
     return (
       <div
         className="inner-container"
-        style={{
-          display: "flex",
-          flexDirection: "column"
-        }}>
-        <div className="sub-header">Malaria Detection Desktop Application (Beta)</div>
+        style={ { display: "flex", flexDirection: "column" } }
+      >
+
+        <div className="sub-header">
+          Malaria Detection Desktop Application (Beta)
+        </div>
+
         <div className="draggable-container">
           <input
+            multiple
             type="file"
-            id="file-browser-input"
-            name="file-browser-input"
-            ref={input => this.fileInput = input}
+            id="file-browser-input" name="file-browser-input"
+            ref={ input => this.fileInput = input }
             onDragOver={(e) => {
               e.preventDefault();
               e.stopPropagation();
+              // console.log(e.target.files)
             }}
-            onDrop={this
-              .onFileLoad
-              .bind(this)}
-            onChange={this
-              .onFileLoad
-              .bind(this)} />
+            // onDrop={ e => this.onFileLoad(e) }
+            onChange={ e => this.onFileLoad(e) }
+          />
+
           <div className="files-preview-container ip-scrollbar">
-            {loadedFiles.map((file, idx) => {
-              return <div className="file" key={idx}>
-                <img src={file.data} />
-                <div className="container">
-                  <span className="progress-bar">
-                    {file.isUploading && <ProgressBar />}
-                  </span>
-                  <span className="remove-btn" onClick={() => this.removeLoadedFile(file)}>
-                    <Icon icon={remove} size={19} />
-                  </span>
-                </div>
-              </div>
-            })}
+            {
+              loadedFiles.map((file, idx) => {
+                return (
+                  <div className="file" key={ idx }>
+                    <img src={ file.data } />
+
+                    <div className="container">
+                      <span className="progress-bar">
+                        {
+                          file.isUploading &&
+                          <ProgressBar/>
+                        }
+                      </span>
+
+                      <span
+                        className="remove-btn"
+                        onClick={ () => this.removeLoadedFile(file) }
+                      >
+                        <Icon icon={ remove } size={ 19 } />
+                      </span>
+                    </div>
+                  </div>
+                )
+              })
+            }
           </div>
+
           <div className="helper-text">Drag and Drop Images Here</div>
+
           <div className="file-browser-container">
             <AnchorButton
               text="Browse"
-              intent={Intent.PRIMARY}
-              minimal={true}
-              onClick={() => this.fileInput.click()} />
+              intent={ Intent.PRIMARY }
+              minimal={ true }
+              onClick={ () => this.fileInput.click() }
+            />
           </div>
         </div>
+
         <AnchorButton
           text="Upload"
-          intent={Intent.SUCCESS}
-          onClick={this
-            .onUpload
-            .bind(this)} />
-        {this.state.dataObject.confidence!='' && this.state.dataObject.diagnosis!='' && <Table data={this.state.dataObject}/>}
+          style={ { marginBottom: '.5em' } }
+          intent={ Intent.SUCCESS }
+          onClick={ () => this.uploadFiles() }
+        />
+
+        {
+          this.state.dataObject.confidence !== '' &&
+          this.state.dataObject.diagnosis !== '' &&
+          <Table data={ this.state.dataObject }/>
+        }
       </div>
     );
   }
 }
+
+export default DraggableUploader
--- react-frontend/src/components/Table.js
+++ react-frontend/src/components/Table.js
@@ -4,43 +4,42 @@ import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-balham.css';
 
 class Table extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            columnDefs: [{
-                headerName: "Patient_ID", field: "Patient_ID", sortable: true
-            }, {
-                headerName: "Confidence", field: "Confidence", sortable: true
-            }, {
-                headerName: "Diagnosis", field: "Diagnosis", sortable: true
-            }],
-            rowData: []
+  constructor(props) {
+    super(props);
+    this.state = {
+      columnDefs: [
+        { headerName: "Filename", field: "Filename", sortable: true },
+        { headerName: "Confidence", field: "Confidence", sortable: true },
+        { headerName: "Diagnosis", field: "Diagnosis", sortable: true }
+      ],
+      rowData: []
+    }
+  }
+
+  componentWillReceiveProps(props) {
+    if (
+      props.data.Confidence != '' && props.data.Diagnosis != '' && this.props.data != this.state.rowData[this.state.rowData.length - 1]
+    ) {
+      this.setState({ rowData: [...this.state.rowData, props.data] })
+    }
+  }
+
+  render() {
+    return (
+      <div
+        className="ag-theme-balham"
+        style={ { height: '500px', width: '560px' } }
+      >
+        {
+          this.state.rowData.length > 0 &&
+          <AgGridReact
+            columnDefs={ this.state.columnDefs }
+            rowData={ this.state.rowData }>
+          </AgGridReact>
         }
-    }
-
-    componentWillReceiveProps(props) {
-        if (props.data.Confidence != '' && props.data.Diagnosis != '' && this.props.data != this.state.rowData[this.state.rowData.length - 1])
-            this.setState({
-                rowData: [...this.state.rowData, props.data]
-            })
-    }
-
-    render() {
-        return (
-            <div
-                className="ag-theme-balham"
-                style={{
-                    height: '500px',
-                    width: '600px'
-                }}
-            >
-                <AgGridReact
-                    columnDefs={this.state.columnDefs}
-                    rowData={this.state.rowData}>
-                </AgGridReact>
-            </div>
-        );
-    }
+      </div>
+    )
+  }
 }
 
-export default Table;
+export default Table
