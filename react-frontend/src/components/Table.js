ca5c38628dcd7eccc31a5adaefc3fe691bab8f45
Htut <phyo.htut@windriver.com>
2020-03-29 00:48:25 -0700

    reverse order

--- react-frontend/src/components/Table.js
+++ react-frontend/src/components/Table.js
@@ -10,31 +10,47 @@ class Table extends Component {
       columnDefs: [
         { headerName: "Filename", field: "Filename", sortable: true },
         { headerName: "Confidence", field: "Confidence", sortable: true },
-        { headerName: "Diagnosis", field: "Diagnosis", sortable: true }
+        { headerName: "Diagnosis", field: "Diagnosis", sortable: true },
+        { headerName: "Timestamp", field: "Timestamp", sortable: true },
       ],
       rowData: []
     }
+
+    this.setRowData = this.setRowData.bind(this)
+  }
+
+  setRowData(props) {
+    let rowData = this.sorted([ ...this.state.rowData, props.data ])
+
+    this.setState({ rowData })
+  }
+
+  sorted = data => {
+    return data.sort((a, b) => {
+      let ta = new Date(a.Timestamp)
+      let tb = new Date(b.Timestamp)
+
+      return tb.getTime() - ta.getTime()
+    })
   }
 
   componentWillReceiveProps(props) {
     if (
-      props.data.Confidence != '' && props.data.Diagnosis != '' && this.props.data != this.state.rowData[this.state.rowData.length - 1]
+      props.data.Confidence !== '' && props.data.Diagnosis !== '' &&
+      this.props.data !== this.state.rowData[this.state.rowData.length - 1]
     ) {
-      this.setState({ rowData: [...this.state.rowData, props.data] })
+      this.setRowData(props)
     }
   }
-
-  // sorted = () => {
-
-  // }
 
   render() {
     return (
       <div
         className="ag-theme-balham"
-        style={{ height: '45vh', width: '500px', background: 'none' } }
+        style={ { height: '45vh', width: '500px', background: 'none' } }
       >
-        {this.state.rowData.length > 0 &&
+        {
+          this.state.rowData.length > 0 &&
           <AgGridReact
             columnDefs={ this.state.columnDefs }
             rowData={ this.state.rowData }>
