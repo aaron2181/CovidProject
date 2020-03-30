e97408abdd5e5967adcee41458c73e4e8536a55e
Htut <phyo.htut@windriver.com>
2020-03-30 16:57:55 -0700

    small tweak

--- react-frontend/src/components/NavBar.js
+++ react-frontend/src/components/NavBar.js
@@ -6,23 +6,26 @@ const NavBar = () => {
     <div>
       <nav className="navbar navbar-expand-md bg-primary navbar-dark">
         <div class="container-fluid">
-          <a className="navbar-brand">
+          <a className="navbar-brand" href="/">
             <img
               src={ require("../statics/images/image_86x60.png") }
               alt="Logo" style={ { width: '80px' } }
             >
             </img>
+
+            <b className="ml-2" style={ { fontSize: '1.35em' } }>
+              COVID-19 Detection
+            </b>
           </a>
 
-          <h3 className="text-white">COVID-19 Detection</h3>
           
-          {/* <div className="navbar-collapse justify-content-end">
-            <a href='/' className="text-white">
+          <div className="navbar-collapse justify-content-end">
+            {/* <a href='/' className="text-white">
               <span style={ { textDecoration: 'underline' } }>
                 Create Account or Login
               </span>
-            </a>
-          </div> */}
+            </a> */}
+          </div>
         </div>
       </nav>
     </div>
